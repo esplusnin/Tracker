3f94a42a50a8a91876f6d45216f26ea6d9c6940d
esplusnin <esplusnin94@yandex.ru>
2023-05-31 13:49:14 +0400

    Добавила презентер к каждому экрану на будущее, для соответствия общей архитектуре

---
+++ Tracker/ViewControllers/Trackers/CreatingTracker/NewTracker/Category/CategoryPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  CategoryPresenter.swift
+//  Tracker
+//
+//  Created by Евгений on 31.05.2023.
+//
+
+import Foundation
---
+++ Tracker/ViewControllers/Trackers/CreatingTracker/NewTracker/Schedule/SchedulePresenter.swift
@@ -0,0 +1,8 @@
+//
+//  SchedulePresenter.swift
+//  Tracker
+//
+//  Created by Евгений on 31.05.2023.
+//
+
+import Foundation
